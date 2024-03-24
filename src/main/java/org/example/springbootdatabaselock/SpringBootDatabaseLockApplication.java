4233fc55889572d64041ad471b3d87cdc07f5f6d
mabyoungg <mabyoungg@gmail.com>
2024-03-24 20:37:58 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/example/springbootdatabaselock/SpringBootDatabaseLockApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootdatabaselock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDatabaseLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDatabaseLockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootdatabaselock/SpringBootDatabaseLockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootdatabaselock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDatabaseLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
